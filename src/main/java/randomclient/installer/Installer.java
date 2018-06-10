15810925986b4853dac09f34a1973425fe2244d9
Conor Byrne <conorbyrne06@icloud.com>
2018-06-10 12:28:16 +0100

    Started work on Installer & Formatted RandomClient Class

--- src/main/java/randomclient/installer/Installer.java
+++ src/main/java/randomclient/installer/Installer.java
@@ -2,8 +2,15 @@ package randomclient.installer;
 
 import javax.swing.*;
 
-public class Installer {
-    public static void main(String... args) {
-        JOptionPane.showMessageDialog(null, "Not done yet!");
+public class Installer extends JFrame {
+
+    private static final int width = 600;
+    private static final int height = 260;
+
+    public Installer() {
+        super.frameInit();
+        setSize(width, height);
+        setResizable(false);
+        setTitle("RandomClient Installer");
     }
 }
